e1d920d0e250b00f326c2c5252e42ce56dedd4df
Marco Ferrati <marco.ferrati@gmail.com>
2025-04-18 00:43:10 +0200

    Add caching datasets

--- AutoSLR.py
+++ AutoSLR.py
@@ -75,23 +75,24 @@ def _(
     terms_to_filter,
 ):
     from typing import List, Tuple
+    from pathlib import Path 
 
     class SemanticScholarData:
         def _fetch_ids(self, search_query: str) -> List[str]:
             ids =[]
-        
+
             url = f"http://api.semanticscholar.org/graph/v1/paper/search/bulk?query={search_query}"
             id_request = requests.get(url).json()
-        
+
             with mo.status.spinner(subtitle="Searching papers..."):
                 while True:
                     if "data" in id_request:
                         for paper in id_request["data"]:
                             ids.append(paper["paperId"])
-        
+
                     if "token" not in id_request:
                         break
-        
+
                     id_request = requests.get(f"{url}&token={id_request['token']}").json()
             return ids
 
@@ -109,7 +110,7 @@ def _(
             references_fields = [f"references.{field}" for field in base_fields]
             citation_fields = [f"citations.{field}" for field in base_fields]
             all_fields = base_fields + references_fields + citation_fields
-        
+
             table_schema = {
                 "paperId": pl.String, 
                 "DOI": pl.String,
@@ -127,7 +128,7 @@ def _(
 
             def extract_DOI(el: dict) -> dict:
                 return el | {"DOI": el.get("externalIds", {}).get("DOI") if isinstance(el.get("externalIds"), dict) else None}
-        
+
             def create_table_row(el: dict) -> dict:
                 return {k: el.get(k, None) for k in table_schema.keys()}
 
@@ -140,44 +141,62 @@ def _(
                     json={"ids": page_ids}
                 ).json()
                 mo.stop("error" in details_request, output=details_request)
-        
+
                 search_df = pl.concat([
                     search_df,
                     pl.from_dicts(map(create_table_row, map(extract_DOI, details_request)), 
                                   infer_schema_length=None)])
-        
+
                 citation_df = pl.concat([
                     citation_df,
                     pl.from_dicts(map(create_table_row, map(extract_DOI, itertools.chain.from_iterable(map(lambda el: el["citations"], details_request)))), 
                                   infer_schema_length=None)])
-        
+
                 reference_df = pl.concat([citation_df, pl.from_dicts(map(create_table_row, map(extract_DOI, itertools.chain.from_iterable(map(lambda el: el["references"], details_request)))),
                                                                      infer_schema_length=None)])
 
             return search_df, citation_df, reference_df
-    
-        def __init__(self, search_query: str):
-            ids = self._fetch_ids(search_query)
-            self.search_df, self.citation_df, self.reference_df = self._fetch_dataframes(ids)
 
-        
+        def __init__(self, search_query: str):
+            self.cache_path = Path(f"cache_datasets/semantic_scholar/d{hash(search_query)}")
+
+            if self.cache_path.exists():
+                self.search_df = pl.read_json(self.cache_path.joinpath("search.json"))
+                self.citation_df = pl.read_json(self.cache_path.joinpath("citation.json"))
+                self.reference_df = pl.read_json(self.cache_path.joinpath("reference.json"))
+            else:
+                ids = self._fetch_ids(search_query)
+                self.search_df, self.citation_df, self.reference_df = self._fetch_dataframes(ids)
+
+                self.cache_path.mkdir(parents=True, exist_ok=True)
+
+                with open(self.cache_path.joinpath("search.json"), "w+") as f:
+                    self.search_df.write_json(f)
+
+                with open(self.cache_path.joinpath("citation.json"), "w+") as f:
+                    self.citation_df.write_json(f)
+
+                with open(self.cache_path.joinpath("reference.json"), "w+") as f:
+                    self.reference_df.write_json(f)
+
+
         def _apply_filters(self, df: pl.DataFrame) -> pl.DataFrame:
             filterd_df = df.filter(
                 (pl.col("title").str.contains_any(terms_to_filter.value)) |
                 (pl.col("abstract").str.contains_any(terms_to_filter.value))
             )
-    
+
             filterd_df = filterd_df.filter(pl.col("citationCount") >= min_citations.value)
-    
+
             if remove_no_doi.value:
                 filterd_df = filterd_df.filter(pl.col("DOI").is_not_null())
-    
+
             return filterd_df
 
         @property
         def filtered_references_df(self) -> pl.DataFrame:
             return self._apply_filters(self.reference_df)
-    
+
         @property
         def filtered_citations_df(self)-> pl.DataFrame:
             return self._apply_filters(self.citation_df)
@@ -193,12 +212,12 @@ def _(
                            "primary_location", 
                            "cited_by_count", 
                            "abstract_inverted_index"]
-        
+
             search = alex.Works().search(search_query).select(fields + ["referenced_works"]).get()
             paper_ids = {p["id"].split("/")[-1] for p in search}
 
             citations_search = alex.Works().filter(cites="|".join(paper_ids)).select(fields).get()
-        
+
             ref_ids = list(set(itertools.chain.from_iterable([p["referenced_works"] for p in search])))
             references_search = itertools.chain.from_iterable(
                 [alex.Works().filter(openalex="|".join(ids)).select(fields).get() 
@@ -206,18 +225,37 @@ def _(
                                                    title="Fetching references...", 
                                                    completion_title=f"Fetch complete")]
             )
-        
+
             def extract_venue(p: dict) -> dict:
                 return {"venue": p.get("primary_location").get("source").get("display_name", None) if isinstance(p.get("primary_location"), dict) and isinstance(p["primary_location"].get("source"), dict) else None}
-        
+
             alex_search_df = pl.from_dicts([ p | extract_venue(p) for p in search]).drop("referenced_works", "primary_location")
             alex_citations_df = pl.DataFrame([ p | extract_venue(p) for p in citations_search]).drop("primary_location")
             alex_references_df = pl.from_dicts([p | extract_venue(p) for p in references_search]).drop("primary_location")
 
             return alex_search_df, alex_citations_df, alex_references_df
-    
+
         def __init__(self, search_query: str):
-            self.search_df, self.citation_df, self.reference_df = self._fetch_dataframes(search_query)
+            self.cache_path = Path(f"cache_datasets/open_alex/d{hash(search_query)}")
+
+            if self.cache_path.exists():
+                self.search_df = pl.read_json(self.cache_path.joinpath("search.json"))
+                self.citation_df = pl.read_json(self.cache_path.joinpath("citation.json"))
+                self.reference_df = pl.read_json(self.cache_path.joinpath("reference.json"))
+            else:
+                self.search_df, self.citation_df, self.reference_df = self._fetch_dataframes(search_query)
+
+                self.cache_path.mkdir(parents=True, exist_ok=True)
+
+                with open(self.cache_path.joinpath("search.json"), "w+") as f:
+                    self.search_df.write_json(f)
+
+                with open(self.cache_path.joinpath("citation.json"), "w+") as f:
+                    self.citation_df.write_json(f)
+
+                with open(self.cache_path.joinpath("reference.json"), "w+") as f:
+                    self.reference_df.write_json(f)
+
 
         def _apply_filters(self, df: pl.DataFrame) -> pl.DataFrame:
             filterd_df = df.filter(
@@ -240,15 +278,15 @@ def _(
         @property
         def filtered_citations_df(self) -> pl.DataFrame:
             return self._apply_filters(self.citation_df)
-        
-        
-    return List, OpenAlexData, SemanticScholarData, Tuple
+
+
+    return List, OpenAlexData, Path, SemanticScholarData, Tuple
 
 
 @app.cell
-def _(OpenAlexData, words):
-    # data = SemanticScholarData(query.value)
-    data = OpenAlexData(" AND ".join(words))
+def _(SemanticScholarData, query):
+    data = SemanticScholarData(query.value)
+    # data = OpenAlexData(" AND ".join(words))
     return (data,)
 
 
